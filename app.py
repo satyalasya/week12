46e7b691534bd180cb81b328f94188a8656125e1
satyalasya <satyalasya06@gmail.com>
2025-11-12 11:52:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('myForm.html')
+def home():
+    return render_template('index.html')
 
-@app.route('/submit', methods=['GET','POST'])
-def submit():
-    # Get data from the form
-    username = request.form['username']
-    return render_template('greeting.html', name=username)
+@app.route('/hotel1')
+def hotel1():
+    return render_template('hotel1.html')
+
+@app.route('/hotel2')
+def hotel2():
+    return render_template('hotel2.html')
+
+@app.route('/hotel3')
+def hotel3():
+    return render_template('hotel3.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= 5000, debug = True)
+    app.run(host='0.0.0.0', port=5000)
