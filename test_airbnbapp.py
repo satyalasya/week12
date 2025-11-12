ecc7efff248d95866813e091aaa818caaa6774fd
satyalasya <satyalasya06@gmail.com>
2025-11-12 12:27:35 +0530

    Update test_airbnbapp.py

--- test_airbnbapp.py
+++ test_airbnbapp.py
@@ -1,3 +1,13 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    # Create a test client using Flask’s built-in testing feature
+    with app.test_client() as client:
+        yield client
+
 def test_home_route(client):
     response = client.get('/')
     assert response.status_code == 200
+    assert b"Find your next stay" in response.data  # optional content check
